7b2528a45b367f82e881e8a5f220ccfe9a3b53aa
ilya <dubikowsky@gmail.com>
2019-10-28 18:18:59 -0400

    todos done

---
+++ build/precache-manifest.589789358353e6288ba2d84dc8f83855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761bfbe8cf19fc7c6ea407d1cf3a836c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8373923ec35bb73d4b1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f4b1ce5dfe40ef021752",
+    "url": "/static/js/2.0481ef67.chunk.js"
+  },
+  {
+    "revision": "e8373923ec35bb73d4b1",
+    "url": "/static/js/main.e63bf597.chunk.js"
+  },
+  {
+    "revision": "c83f1368d311e59c57b4",
+    "url": "/static/js/runtime-main.3ee827b8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
